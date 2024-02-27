1ab4e66c5162c82774a544d84d48e1a53778282f
Unai de la Mata <unaimax09@gmail.com>
2024-02-27 16:40:20 +0100

    Añadidos premios y contador de puntuacion y cambiadas texturas y skybox

--- Assets/Scripts/JugadorBola.cs
+++ Assets/Scripts/JugadorBola.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class JugadorBola : MonoBehaviour
 {
@@ -10,10 +11,13 @@ public class JugadorBola : MonoBehaviour
     public Camera camara;
     public GameObject suelo;
     public float velocidad;
+    public GameObject Estrella;
+    public Text texto;
     //private
     private Vector3 offset;
     private float Valx, Valz;
     private Vector3 DireccionActual;
+    private int estrellas = 0;
 
     void Start()
     {
@@ -35,7 +39,8 @@ public class JugadorBola : MonoBehaviour
     void CrearSueloInicial(){
         for(int i = 0; i < 3; i++){
             Valz += 6.0f;
-            Instantiate(suelo, new Vector3(Valx, 0, Valz), Quaternion.identity);
+            GameObject newSuelo = Instantiate(suelo, new Vector3(Valx, 0, Valz), Quaternion.identity);
+            GenerarPremio(newSuelo.transform.position);
         }
     }
     private void OnCollisionExit(Collision other) {
@@ -52,7 +57,8 @@ public class JugadorBola : MonoBehaviour
         else{
             Valz += 6.0f;
         }
-        Instantiate(suelo, new Vector3(Valx, 0, Valz), Quaternion.identity);
+        GameObject newSuelo = Instantiate(suelo, new Vector3(Valx, 0, Valz), Quaternion.identity);
+        GenerarPremio(newSuelo.transform.position);
         yield return new WaitForSeconds(2);
         suelo.gameObject.GetComponent<Rigidbody>().isKinematic = false;
         suelo.gameObject.GetComponent<Rigidbody>().useGravity = true;
@@ -60,12 +66,33 @@ public class JugadorBola : MonoBehaviour
         Destroy(suelo);
 
     }
+
+     void GenerarPremio(Vector3 position)
+    {
+        // Generar premio aleatoriamente
+        if (Random.value > 0.5f)
+        {
+            Vector3 premioPosition = position + Vector3.up * 0.5f; // Altura sobre el suelo
+            GameObject newPremio = Instantiate(Estrella, premioPosition, Quaternion.identity);
+            newPremio.transform.Rotate(new Vector3(0, 0, 45));
+        }
+    }
+
     void CambiarDireccion(){
         if(DireccionActual == Vector3.forward){
             DireccionActual = Vector3.right;
         }
         else{
             DireccionActual = Vector3.forward;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("premio")){
+            Destroy(other.gameObject);
+            Debug.Log("Ha tocado una estrella");
+            estrellas++;
+            texto.text = "Puntuacion: " + estrellas;
         }
     }
 }
